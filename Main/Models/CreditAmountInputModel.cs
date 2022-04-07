63aa0ac4f236ab640bae36dd39f931e2bdb51120
Riyaz0101 <102675231+Riyaz0101@users.noreply.github.com>
2022-04-07 18:06:08 +0530

    Main Project

---
+++ Main/Models/CreditAmountInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Main.Models
+{
+    public class CreditAmountInputModel
+    {
+        //[Required(ErrorMessage = "The Amount is required")]
+        //[Range(0, Int64.MaxValue, ErrorMessage = "The Amount should not contain characters")]
+        public string creditAmount { get; set; }
+    }
+}
