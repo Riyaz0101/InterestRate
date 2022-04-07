fef447e24e18a7eec2d9ee8b4bdd0b01c0375f3b
Riyaz0101 <102675231+Riyaz0101@users.noreply.github.com>
2022-04-07 18:04:15 +0530

    API Added

---
+++ InterestRate/Controllers/RateController.cs
@@ -0,0 +1,27 @@
+﻿using InterestRate.Models;
+using InterestRates.Data.Interfaces;
+using InterestRates.Data.Repositories;
+using Main.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace InterestRate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RateController : ControllerBase
+    {
+        private IinterestRateRepository interestRateRepository = new InterestRateRepository();
+
+       [HttpPost]
+        public InterestDetailsModel Post([FromBody] CreditAmountInputModel obj)
+        {
+            var data = interestRateRepository.GetInterestDetail(Convert.ToDouble(obj.creditAmount));
+            if (data == null)
+            {
+                return new InterestDetailsModel();
+            }
+            return data;
+        }
+    }
+}
